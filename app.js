40c8c7b976fcbdbe9e0845b46215d0da560a46b8
Arkane <niflheim.exease@gmail.com>
2023-07-07 01:34:39 -0500

    inital commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+let p5rData = JSON.parse(JSON.stringify(yaml.load(fs.readFileSync('./data/p5r.yml', 'utf8')), null, 2));
+console.log(p5rData.persona.arsene.trait);
+
