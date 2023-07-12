a0637ddc3d8d8cb92145432a0c35766f89981d5e
Arkane <niflheim.exease@gmail.com>
2023-07-11 22:23:00 -0500

    add physical skills to p5r data

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 
 let p5rData = JSON.parse(JSON.stringify(yaml.load(fs.readFileSync('./data/p5r.yml', 'utf8')), null, 2));
-console.log(p5rData.persona.arsene.trait);
+console.log(p5rData.persona.arsene);
 
